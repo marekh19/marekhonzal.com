8ec00003429d2a423b1bba1bee80d6bf24046567
Marek Honzal <marek.honzal@gmail.com>
2025-07-24 23:31:12 +0200

    chore: add astro icon and sitemap integrations

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import preact from '@astrojs/preact'
 import mdx from '@astrojs/mdx'
+import icon from 'astro-icon'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://marekhonzal.com',
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [preact(), mdx()],
-  trailingSlash: 'ignore',
+  integrations: [preact(), mdx(), icon(), sitemap({ lastmod: new Date() })],
+  trailingSlash: 'never',
   markdown: {
     shikiConfig: {
       themes: {
