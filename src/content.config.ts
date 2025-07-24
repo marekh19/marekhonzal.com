33785489cb4c7b127c757f207595a2422edd4822
Marek Honzal <marek.honzal@gmail.com>
2025-07-24 23:30:41 +0200

    feat: update content collection schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,30 @@
 import { z, defineCollection } from 'astro:content'
 import { glob } from 'astro/loaders'
 
-const blogSchema = z.object({
-  title: z.string(),
-  excerpt: z.string(),
-  date: z.date(),
-  draft: z.boolean().optional().default(false),
+const blogCollection = defineCollection({
+  loader: glob({ pattern: '**/*.{mdx,md}', base: './src/content/blog' }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      excerpt: z.string(),
+      date: z.date(),
+      isDraft: z.boolean().optional().default(false),
+      isFeatured: z.boolean().optional().default(false),
+      category: z.enum(['dev', 'beyond']).optional().default('dev'),
+      tags: z.array(z.string()).optional(),
+      thumbnail: image(),
+    }),
 })
 
-export type BlogSchema = z.infer<typeof blogSchema>
-
-const blog = defineCollection({
-  loader: glob({ pattern: '**/*.mdx', base: './src/content/blog' }),
-  schema: blogSchema,
+const projectsCollection = defineCollection({
+  loader: glob({ pattern: '**/*.{mdx,md}', base: './src/content/projects' }),
+  schema: z.object({
+    name: z.string(),
+    summary: z.string(),
+  }),
 })
 
-export const collections = { blog }
+export const collections = {
+  blog: blogCollection,
+  projects: projectsCollection,
+}
