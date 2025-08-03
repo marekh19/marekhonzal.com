5295243fdb27d40ae062c90180ffe5507c2f8e6b
Marek Honzal <marek.honzal@gmail.com>
2025-08-03 21:25:18 +0200

    chore: setup prettier plugin sort imports

--- prettier.config.js
+++ prettier.config.js
@@ -6,8 +6,21 @@ const config = {
   printWidth: 90,
   semi: false,
   singleQuote: true,
-  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  plugins: [
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss',
+    '@ianvs/prettier-plugin-sort-imports',
+  ],
   tailwindFunctions: ['cva', 'cn'],
+  importOrder: [
+    '^astro(:.*)?$',
+    '^(react|react-dom|preact(?:/.*)?)$',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/.*$',
+    '',
+    '^[./]',
+  ],
   overrides: [
     {
       files: '*.astro',
