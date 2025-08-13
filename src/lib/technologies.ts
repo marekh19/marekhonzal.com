ea5d5dd9bcb214fa092abcb763618e7793cbceb8
Marek Honzal <79986994+marekh19@users.noreply.github.com>
2025-08-13 22:27:26 +0200

    refactor(projects): implement union for icon - either iconName or direct icon component

--- src/lib/technologies.ts
+++ src/lib/technologies.ts
@@ -1,3 +1,6 @@
+import ModuleFederation from '@/assets/logos/module-federation.svg'
+import TurboRepo from '@/assets/logos/turborepo.svg'
+
 import type { Technology } from './types'
 
 type TechnologySlug = keyof typeof TECHNOLOGIES
@@ -7,6 +10,8 @@ export const TECHNOLOGIES = {
   typescript: { title: 'TypeScript', iconName: 'logos:typescript-icon' },
   vite: { title: 'Vite', iconName: 'logos:vitejs' },
   nextjs: { title: 'Next.js', iconName: 'logos:nextjs-icon' },
+  moduleFederation: { title: 'Module federation', Icon: ModuleFederation },
+  turboRepo: { title: 'Turborepo', Icon: TurboRepo },
 } as const satisfies Record<string, Technology>
 
 export const TECHNOLOGY_SLUGS = Object.keys(TECHNOLOGIES) as [
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,5 @@
 import type { CollectionEntry } from 'astro:content'
+import type { AstroComponentFactory } from 'astro/runtime/server/index.js'
 
 export type Project = CollectionEntry<'projects'>
 export type ProjectData = Project['data']
@@ -8,5 +9,11 @@ export type PostData = Post['data']
 
 export type Technology = {
   title: string
-  iconName: `logos:${string}`
-}
+} & (
+  | {
+      iconName: `logos:${string}`
+    }
+  | {
+      Icon: AstroComponentFactory
+    }
+)
