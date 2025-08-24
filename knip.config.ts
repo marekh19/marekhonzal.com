58c7fd3e9f6d7cdf0a7d660d92f356e2e97fb86d
Marek Honzal <79986994+marekh19@users.noreply.github.com>
2025-08-24 23:13:32 +0200

    chore: setup knip

---
+++ knip.config.ts
@@ -0,0 +1,27 @@
+import type { KnipConfig } from 'knip'
+
+export default {
+  astro: {
+    config: ['astro.config.{js,cjs,mjs,ts,mts}'],
+    entry: [
+      'src/content/config.ts',
+      'src/content.config.ts',
+      'src/pages/**/*.{astro,mdx,js,ts}',
+      '!src/pages/**/_*',
+      '!src/pages/**/_*/**',
+      'src/content/**/*.mdx',
+    ],
+    project: ['src/**/*'],
+  },
+  ignore: ['src/scripts/copy-code.ts'],
+  ignoreDependencies: [
+    '@iconify-json/logos',
+    '@iconify-json/lucide',
+    '@iconify-json/ph',
+    'sharp',
+    '@tailwindcss/typography',
+    'tailwindcss',
+    'wrangler',
+    'daisyui',
+  ],
+} satisfies KnipConfig
