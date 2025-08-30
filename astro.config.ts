14bd1fe44ee72e3b91df95924e10e6a8e1c6aa3f
Marek Honzal <79986994+marekh19@users.noreply.github.com>
2025-08-30 22:45:56 +0200

    feat(toc): implement dynamic animated table of contents

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 import rehypeMermaid from 'rehype-mermaid'
+import sectionize from 'remark-sectionize'
 
 // https://astro.build/config
 export default defineConfig({
@@ -30,5 +31,6 @@ export default defineConfig({
       },
     },
     rehypePlugins: [[rehypeMermaid, { strategy: 'img-svg', dark: true }]],
+    remarkPlugins: [sectionize],
   },
 })
