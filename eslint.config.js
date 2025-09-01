6129acd03802fcc285f791da2395bab5c43c9021
Marek Honzal <79986994+marekh19@users.noreply.github.com>
2025-09-01 22:43:41 +0200

    chore: ignore .wrangler in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,14 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
-    ignores: ['**/dist', '**/node_modules', '**/.astro', '**/.github', '**/.changeset'],
+    ignores: [
+      '**/dist',
+      '**/node_modules',
+      '**/.astro',
+      '**/.github',
+      '**/.changeset',
+      '**/.wrangler',
+    ],
   },
 
   // Global config
