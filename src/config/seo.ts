bdc1ab362872a0ff97f2af9c86f10ad07bc0af27
Marek Honzal <79986994+marekh19@users.noreply.github.com>
2025-09-02 20:51:42 +0200

    chore: replace em dashes with hyphens

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -2,7 +2,7 @@ export const defaultSeo = {
   baseTitle: 'Marek Honzal',
   pageTitle: (title: string) => `${title} | ${defaultSeo.baseTitle}` as const,
   metaDescription:
-    "I'm Marek Honzal, a frontend developer writing about architecture, state management, and developer experience — plus lessons from side projects",
+    "I'm Marek Honzal, a frontend developer writing about architecture, state management, and developer experience - plus lessons from side projects",
 } as const
 
 export const OG_FALLBACK_SLUG = 'base'
