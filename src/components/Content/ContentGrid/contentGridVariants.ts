2f032c51b024f1a6b3d3f61ae67f7936ff8a1ca0
Marek Honzal <79986994+marekh19@users.noreply.github.com>
2025-09-11 18:35:28 +0200

    feat: update responsive behavior of ContentGrid component

--- src/components/Content/ContentGrid/contentGridVariants.ts
+++ src/components/Content/ContentGrid/contentGridVariants.ts
@@ -1,15 +1,16 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 
-export const contentGridVariants = cva(['grid grid-cols-1 gap-x-8 gap-y-12'], {
-  variants: {
-    type: {
-      article: ['min-[600px]:grid-cols-2', 'lg:grid-cols-3'],
-      project: ['lg:gap-y-8'],
+export const contentGridVariants = cva(
+  ['grid grid-cols-1 min-[600px]:grid-cols-2 gap-x-8 gap-y-12'],
+  {
+    variants: {
+      type: {
+        article: ['lg:grid-cols-3'],
+        project: ['lg:grid-cols-1', 'lg:gap-y-8'],
+      },
     },
+    defaultVariants: { type: 'article' },
   },
-  defaultVariants: {
-    type: 'article',
-  },
-})
+)
 
 export type ContentGridVariant = VariantProps<typeof contentGridVariants>
