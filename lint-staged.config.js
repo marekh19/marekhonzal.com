6aff52c8c37b02b14f0cc5eebe1f6b75f33cc8ac
Marek Honzal <79986994+marekh19@users.noreply.github.com>
2025-09-12 23:03:30 +0200

    chore: add eslint to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 const config = {
-  './src/**/*.{ts,tsx,js,cjs,json,astro,md,mdx}': ['prettier --write'],
+  './src/**/*.{ts,tsx,js,cjs,json,astro,md,mdx}': ['prettier --write', 'eslint'],
 }
 
 export default config
